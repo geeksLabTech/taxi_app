dc5a5a313130496c71cab4f97c61ed3378453d42
Daniel <danielcardenascabrera2016@gmail.com>
2022-06-11 01:32:30 -0400

    Create_button_for_login_and_sign_in_pages

---
+++ lib/src/widgets/blue_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BlueButton extends StatelessWidget {
+
+  final String text;
+  final Function onPressed;
+
+  const BlueButton({
+    Key? key, 
+    required this.text, 
+    required this.onPressed
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+
+      style: ButtonStyle(
+        elevation: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return 0;
+          }
+          return 5;
+        }),
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Colors.blue[600];
+          }
+          return Colors.blue[200];
+        }),
+        shape: MaterialStateProperty.all(StadiumBorder()),
+      ),
+        
+      onPressed: (){},
+        child: Container(
+          width: double.infinity,
+          height: 55,
+          child: Center(
+            child: Text( text , style: const TextStyle( color: Colors.white, fontSize: 17 )),
+          ),
+        ),
+    );
+  }
+
+}
