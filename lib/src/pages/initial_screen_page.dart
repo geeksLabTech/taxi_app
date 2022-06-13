34f2d16f09d7b357e901b07990d81d8782e656b7
JavierOramas <javiale2000@gmail.com>
2022-06-13 17:21:46 -0400

    pagina scrolleable del home no pinchando

--- lib/src/pages/initial_screen_page.dart
+++ lib/src/pages/initial_screen_page.dart
@@ -70,6 +70,19 @@ class InitialScreenPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
+                SizedBox.expand(child: DraggableScrollableSheet(
+                  builder: (BuildContext context,
+                      ScrollController scrollController) {
+                    return Container(
+                        color: Colors.blue[100],
+                        child: ListView.builder(
+                            controller: scrollController,
+                            itemCount: 25,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(title: Text('Item $index'));
+                            }));
+                  },
+                )),
               ],
             )
           ],
--- lib/src/styles/style_drawer_items.dart
+++ lib/src/styles/style_drawer_items.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 final drawerListTileStyleText = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w500,
-  color: Colors.blue[900],
+  color: Colors.indigo,
 );
 
 final drawerPersonalInfoStyleText = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w500,
-  color: Colors.blue[900],
+  color: Colors.indigo,
 );
