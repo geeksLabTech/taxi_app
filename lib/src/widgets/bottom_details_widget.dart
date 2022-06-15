8be64a8588a8141921a73ddc0e752aee9a856a6d
JavierOramas <javiale2000@gmail.com>
2022-06-15 07:44:52 -0400

    drawer rounded borders

--- lib/src/widgets/bottom_details_widget.dart
+++ lib/src/widgets/bottom_details_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class BottomDetails extends StatelessWidget {
@@ -10,115 +12,118 @@ class BottomDetails extends StatelessWidget {
       minChildSize: .1,
       maxChildSize: .6,
       builder: (BuildContext context, ScrollController scrollController) {
-        return Container(
-          color: Colors.lightBlue[100],
-          child: ListView(
-            controller: scrollController,
-            children: [
-              Container(
-                child: ListTile(
-                  title: Center(child: Text('Trip Details')),
-                  trailing: Icon(Icons.close),
-                  onTap: () {},
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: Colors.lightBlue[100],
+            child: ListView(
+              controller: scrollController,
+              children: [
+                Container(
+                  child: ListTile(
+                    title: Center(child: Text('Trip Details')),
+                    trailing: Icon(Icons.close),
+                    onTap: () {},
+                  ),
                 ),
-              ),
-              Divider(),
-              Container(
-                child: ListTile(
-                  title: Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image(
-                          image: AssetImage(
-                            'assets/images/user.png',
-                          ),
-                          width: 40,
-                          height: 40,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Text("John Doe"),
-                            Row(
-                              children: [
-                                Icon(Icons.star, color: Colors.yellow),
-                                Text("4.9"),
-                              ],
+                Divider(),
+                Container(
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image(
+                            image: AssetImage(
+                              'assets/images/user.png',
                             ),
+                            width: 40,
+                            height: 40,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Text("John Doe"),
+                              Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.yellow),
+                                  Text("4.9"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text("Final Cost"), // grey small
+                            Text("\$100"), //black big
                           ],
                         ),
-                      ),
-                      Column(
-                        children: [
-                          Text("Final Cost"), // grey small
-                          Text("\$100"), //black big
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        children: [
-                          Text("Time"), // grey small
-                          Text("00:15"), //black big
-                        ],
-                      ),
-                    ],
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Divider(),
-              Container(
-                child: ListTile(
-                  title: Text('Trip',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  title: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.location_on),
-                          Text("Universidad de La Habana"),
-                          Spacer(),
-                          Text("9:50 AM"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(Icons.more_vert),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(Icons.trip_origin),
-                          Text("Teatro Nacional"),
-                          Spacer(),
-                          Text("10:05 AM"),
-                        ],
-                      ),
-                    ],
+                        Spacer(),
+                        Column(
+                          children: [
+                            Text("Time"), // grey small
+                            Text("00:15"), //black big
+                          ],
+                        ),
+                      ],
+                    ),
+                    onTap: () {},
                   ),
                 ),
-              ),
-              Divider(),
-              Container(
-                child: ListTile(
-                  title: Center(
-                    child: Text("Status: Finished",
+                Divider(),
+                Container(
+                  child: ListTile(
+                    title: Text('Trip',
                         style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold)),
+                            fontSize: 20, fontWeight: FontWeight.bold)),
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  child: ListTile(
+                    title: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on),
+                            Text("Universidad de La Habana"),
+                            Spacer(),
+                            Text("9:50 AM"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.more_vert),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.trip_origin),
+                            Text("Teatro Nacional"),
+                            Spacer(),
+                            Text("10:05 AM"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(),
+                Container(
+                  child: ListTile(
+                    title: Center(
+                      child: Text("Status: Finished",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
