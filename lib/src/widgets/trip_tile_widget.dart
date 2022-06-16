c4bff9282a351185a2fb6ab4ca0c8394d37784f4
JavierOramas <javiale2000@gmail.com>
2022-06-16 01:12:33 -0400

    tiles working fine

--- lib/src/widgets/trip_tile_widget.dart
+++ lib/src/widgets/trip_tile_widget.dart
@@ -7,49 +7,85 @@ class TripTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // ListTile(
-    //   title: Column(children: [
-    //     Row(children: [
-    //       ClipRRect(
-    //         borderRadius: BorderRadius.circular(10),
-    //         child: const Image(
-    //           image: AssetImage(
-    //             'assets/images/user.png',
-    //           ),
-    //           width: 40,
-    //           height: 40,
-    //         ),
-    //       ),
-    //       Container(
-    //         margin: EdgeInsets.all(10),
-    //         child: Column(
-    //           children: [
-    //             Text("John Doe"),
-    //             Row(
-    //               children: [
-    //                 Icon(Icons.star, color: Colors.yellow),
-    //                 Text("4.9"),
-    //               ],
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //       Column(
-    //         children: [
-    //           Text("Final Cost"), // grey small
-    //           Text("\$100"), //black big
-    //         ],
-    //       ),
-    //       Spacer(),
-    //       Column(
-    //         children: [
-    //           Text("Time"), // grey small
-    //           Text("00:15"), //black big
-    //         ],
-    //       ),
-    //     ]),
-    //   ]),
-    // );
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(50),
+      child: Container(
+        color: Colors.grey[200],
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(20),
+        child: Column(children: [
+          Row(children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: const Image(
+                image: AssetImage(
+                  'assets/images/user.png',
+                ),
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text("John Doe"),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.yellow),
+                      Text("4.9"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Text("Final Cost"), // grey small
+                Text("\$100"), //black big
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                Text("Time"), // grey small
+                Text("00:15"), //black big
+              ],
+            ),
+          ]),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.location_on),
+                        Text("Universidad de La Habana"),
+                        Spacer(),
+                        Text("9:50 AM"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.more_vert),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.trip_origin),
+                        Text("Teatro Nacional"),
+                        Spacer(),
+                        Text("10:05 AM"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
