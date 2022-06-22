f489fb7878fe1a61afe243339bf1a0d73ed06426
JavierOramas <javiale2000@gmail.com>
2022-06-22 11:41:03 -0400

    driver provider

--- lib/src/data/providers/remote/driver_provider.dart
+++ lib/src/data/providers/remote/driver_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:taxi_app/src/data/models/driver_model.dart';
 import 'package:taxi_app/src/services/api_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
@@ -6,19 +9,82 @@ class DriverProvider {
   final baseUrl = '/driver';
   final apiService = GetIt.I<ApiService>();
 
-  getAllDrivers() {
-    return http.get(Uri.parse('http://194.36.189.148:8000/driver/'));
+  getAllDrivers() async {
+    final response =
+        await http.get(Uri.parse('http://194.36.189.148:8000/driver/'));
+    if (response.statusCode == 200) {
+      Iterable l = json.decode(response.body);
+      final drivers = List<Driver>.from(l.map((model) {
+        return Driver(
+            id: model['id'],
+            ci: model['ci'],
+            name: model['name'],
+            phoneNumber: model['phoneNumber'],
+            password: model['password']);
+      }));
+      return drivers;
+    }
+    throw Exception('Failed to load data');
   }
 
-  getDriver() {}
+  getDriver(int id) async {
+    final response = await http
+        .get(Uri.parse('http://194.36.189.148:8000/driver/' + id.toString()));
+    if (response.statusCode == 200) {
+      return Driver.fromJson(json.decode(response.body));
+    }
+    throw Exception('Failed to load data');
+  }
 
-  getDriverByPhoneNumber() {}
+  getDriverByPhoneNumber(String phoneNumber) async {
+    final response = await http.get(Uri.parse(
+        'http://194.36.189.148:8000/driver/phone_number/' + phoneNumber));
+    if (response.statusCode == 200) {
+      return Driver.fromJson(json.decode(response.body));
+    }
+    throw Exception('Failed to load data');
+  }
 
-  getDriverByCi() {}
+  getDriverByCi(String ci) async {
+    final response =
+        await http.get(Uri.parse('http://194.36.189.148:8000/driver/ci/' + ci));
+    if (response.statusCode == 200) {
+      return Driver.fromJson(json.decode(response.body));
+    }
+    throw Exception('Failed to load data');
+  }
 
-  createDriver() {}
+  createDriver(Driver driver) async {
+    // post request to create driver
+    final serializedDriver = jsonEncode(driver);
+    final response = await http.post(
+        Uri.parse('http://194.36.189.148:8000/driver/'),
+        body: serializedDriver);
+    if (response.statusCode == 200) {
+      return true;
+    }
+    throw Exception('Failed to load data');
+  }
 
-  updateDriver() {}
+  updateDriver(Driver driver) async {
+    // put request to update driver
+    final serializedDriver = jsonEncode(driver);
+    final response = await http.put(
+        Uri.parse('http://194.36.189.148:8000/driver/'),
+        body: serializedDriver);
+    if (response.statusCode == 200) {
+      return true;
+    }
+    throw Exception('Failed to load data');
+  }
 
-  deleteDriver() {}
+  deleteDriver(int id) async {
+    // delete request to delete driver
+    final response = await http.delete(
+        Uri.parse('http:///194.36.189.148:8000/driver/' + id.toString()));
+    if (response.statusCode == 200) {
+      return true;
+    }
+    throw Exception('Failed to load data');
+  }
 }
